8f215d098fd4d37c7fb4ea8b855ee1e0f16a29cd
Ethan Chan <echan@nianticlabs.com>
2021-11-29 10:46:39 +0900

    Remove unnecessary code.

--- keyboards/gray_studio/think65/solder/solder.c
+++ keyboards/gray_studio/think65/solder/solder.c
@@ -14,23 +14,3 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "solder.h"
-
-// Optional override functions below.
-// You can leave any or all of these undefined.
-// These are only required if you want to perform custom actions.
-
-
-void matrix_init_kb(void) {
-  // put your keyboard start-up code here
-  // runs once when the firmware starts up
-
-  setPinOutput(C7);
-  matrix_init_user();
-}
-
-bool led_update_kb(led_t led_state) {
-  if(led_update_user(led_state)) {
-    writePin(C7, !led_state.caps_lock);
-  }
-  return true;
-}
